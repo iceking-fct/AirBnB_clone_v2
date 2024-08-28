b633ea11c0234293caf3218b7dbcfa24165d851c
iceking-fct <miracleamajama14@gmail.com>
2024-08-28 05:43:20 +0000

    0-hello_route.py solved

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """defines the base app route, and prints hello hbnb at the root"""
     return "Hello HBNB!"
